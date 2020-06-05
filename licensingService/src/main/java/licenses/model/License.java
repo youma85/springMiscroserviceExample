d0118bb09297d94643366a17bc27ad3702a04434
Rafii <rafiiyoussef@gmail.com>
2020-06-05 14:04:40 +0100

    adding licence service and repo

--- licensingService/src/main/java/licenses/model/License.java
+++ licensingService/src/main/java/licenses/model/License.java
@@ -1,18 +1,26 @@
 package licenses.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "licenses")
 public class License{
-    private String id;
+    @Id
+    @Column(name = "license_id", nullable = false)
+    private String licenseId;
+    @Column(name = "organization_id", nullable = false)
     private String organizationId;
+    @Column(name = "product_name", nullable = false)
     private String productName;
+    @Column(name = "license_type", nullable = false)
     private String licenseType;
+    @Column(name="comment")
+    private String comment;
 
-    public String getId() {
-        return id;
-    }
 
-    public void setId(String id) {
-        this.id = id;
-    }
 
     public String getOrganizationId() {
         return organizationId;
@@ -30,18 +38,6 @@ public class License{
         this.productName = productName;
     }
 
-    public String getLicenseType() {
-        return licenseType;
-    }
-
-    public void setLicenseType(String licenseType) {
-        this.licenseType = licenseType;
-    }
-
-    public License withId(String id){
-        this.setId( id );
-        return this;
-    }
 
     public License withOrganizationId(String organizationId){
         this.setOrganizationId(organizationId);
@@ -53,11 +49,35 @@ public class License{
         return this;
     }
 
+    public void setLicenseId(String licenseId) {
+        this.licenseId = licenseId;
+    }
+
+    public String getLicenseId() {
+        return licenseId;
+    }
+
     public License withLicenseType(String licenseType){
         this.setLicenseType(licenseType);
         return this;
     }
 
 
+    public void setLicenseType(String licenseType) {
+        this.licenseType = licenseType;
+    }
 
+    public License withId(String id){
+        this.setLicenseId(id);
+        return this;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public License withComment(String comment){
+        this.setComment(comment);
+        return this;
+    }
 }
