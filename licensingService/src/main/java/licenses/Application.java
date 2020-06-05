04f4462f78d8af6bc66a1cb08da0b2bbb093497d
Rafii <rafiiyoussef@gmail.com>
2020-06-05 14:06:37 +0100

    auto refresh the config

--- licensingService/src/main/java/licenses/Application.java
+++ licensingService/src/main/java/licenses/Application.java
@@ -2,9 +2,11 @@ package licenses;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 //@SpringBootApplication tells the Spring Boot framework that this is the bootstrap class for the project
 @SpringBootApplication
+@RefreshScope
 public class Application {
     public static void main(String[] args) {
         // Call to start the entire Spring Boot service
